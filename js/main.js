994e5f883e4a51b97ad40db7b5361d1af17ce6f7
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-09-24 16:17:25 -0700

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Making text disappear
 
-document.querySelector("#text-1").addEventListener("click", function(){
-    document.querySelector("#text-1").style.display = "none";
+document.querySelector("#text-0").addEventListener("click", function(){
+    document.querySelector("#text-1").style.visibility = "visible";
 })
