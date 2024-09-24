03ff6954da9beac69c9fab4196d5fd3e4dbdcc90
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-09-24 16:09:11 -0700

    new

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-//Making text appear
+//Making text disappear
+
+document.querySelector("#text-1").addEventListener("click", function(){
+    document.querySelector("#text-1").style.display = "none";
+})
