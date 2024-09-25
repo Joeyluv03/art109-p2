2571b4ef213d49a47581b233d04e9c7fca0da576
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-09-24 19:46:36 -0700

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 //Making text disappear
 
-document.querySelector("#text-0").addEventListener("click", function(){
-    document.querySelector("#text-1").style.visibility = "visible";
+// document.querySelector("#text-0").addEventListener("click", function(){
+//     document.querySelector("#text-1").style.visibility = "visible";
+// })
+document.querySelector("#text-1").addEventListener("click", function(){
+    document.querySelector("#text-2").style.visibility = "visible";
+})
+
+document.querySelector("#text-2").addEventListener("click", function(){
+    document.querySelector("#text-3").style.visibility = "visible";
+})
+
+document.querySelector("#text-3").addEventListener("click", function(){
+    document.querySelector("#text-4").style.visibility = "visible";
+})
+
+document.querySelector("#text-4").addEventListener("click", function(){
+    document.querySelector("#text-5").style.visibility = "visible";
 })
