3ac60fdbb2abbdd8b8f705bf2fb6b79ffda20fe1
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-09-26 12:56:25 -0700

    new text

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@
 // document.querySelector("#text-0").addEventListener("click", function(){
 //     document.querySelector("#text-1").style.visibility = "visible";
 // })
-document.querySelector("#text-1").addEventListener("click", function(){
+document.querySelector("#text-1").addEventListener("click", function () {
     document.querySelector("#text-2").style.visibility = "visible";
 })
 
-document.querySelector("#text-2").addEventListener("click", function(){
+document.querySelector("#text-2").addEventListener("click", function () {
     document.querySelector("#text-3").style.visibility = "visible";
 })
 
-document.querySelector("#text-3").addEventListener("click", function(){
+document.querySelector("#text-3").addEventListener("click", function () {
     document.querySelector("#text-4").style.visibility = "visible";
 })
 
-document.querySelector("#text-4").addEventListener("click", function(){
+document.querySelector("#text-4").addEventListener("click", function () {
     document.querySelector("#text-5").style.visibility = "visible";
 })
